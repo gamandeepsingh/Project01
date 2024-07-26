321f8ebe3acf328d2d0f7b0502bd76884165bd3b
TheGhost <116256043+gamandeepsingh@users.noreply.github.com>
2024-07-26 23:42:38 +0530

    chore: Update CORS origin to use the correct frontend URL

--- Backend/app.js
+++ Backend/app.js
@@ -9,7 +9,7 @@ const app = express()
 dotenv.config()
 
 app.use(cors({
-    origin:[process.env.FRONTEND_URL],
+    origin:"https://dineme.vercel.app/",
     methods:["POST"],
     credentials:true
 }))
